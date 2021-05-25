fb8c11c25a647540739b5be3e3b34b03107cfe67
vilaksh01 <44412828+vilaksh01@users.noreply.github.com>
2021-05-25 09:18:39 +0530

    Add files via upload

---
+++ Mosquito-Sensing-Control/knowledgepack/sensiml/inc/model_json.h
@@ -0,0 +1,8 @@
+#ifndef __MODEL_JSON_H__
+#define __MODEL_JSON_H__
+
+const char recognition_model_string_json[] = {"{\"NumModels\":1,\"ModelIndexes\":{\"0\":\"Pipeline_2_rank_4\"},\"ModelDescriptions\":[{\"Name\":\"Pipeline_2_rank_4\",\"ClassMaps\":{\"1\":\"AedesAegypti\",\"2\":\"AedesAlbopictus\",\"3\":\"AnophelesMinimus\",\"4\":\"Background\",\"5\":\"CulexTarsalis\",\"0\":\"Unknown\"},\"ModelType\":\"PME\"}]}"};
+
+int recognition_model_string_json_len = sizeof(recognition_model_string_json);
+
+#endif /* __MODEL_JSON_H__ */
