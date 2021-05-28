d867cfac5cb57f789239f9e74976880300762d80
vilaksh01 <44412828+vilaksh01@users.noreply.github.com>
2021-05-28 12:11:14 +0530

    Add files via upload

---
+++ Pest-Sensing-Control/knowledgepack/sensiml/inc/model_json.h
@@ -0,0 +1,8 @@
+#ifndef __MODEL_JSON_H__
+#define __MODEL_JSON_H__
+
+const char recognition_model_string_json[] = {"{\"NumModels\":1,\"ModelIndexes\":{\"0\":\"Pipeline_1_rank_3\"},\"ModelDescriptions\":[{\"Name\":\"Pipeline_1_rank_3\",\"ClassMaps\":{\"1\":\"background\",\"2\":\"cricket\",\"0\":\"Unknown\"},\"ModelType\":\"PME\"}]}"};
+
+int recognition_model_string_json_len = sizeof(recognition_model_string_json);
+
+#endif /* __MODEL_JSON_H__ */
